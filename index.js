be48210725621093f01a6bf570b83a05e42a865c
vasili311 <116735770+vasili311@users.noreply.github.com>
2022-12-01 18:51:17 +0400

    Update index.js

--- index.js
+++ index.js
@@ -23,3 +23,55 @@ const users = {
      } = users ;
 
  console.log(city);
+
+const user = {
+    name : 'Vasili',
+    lastName: 'Vakhtangishvili',
+    city : 'Tbilisi',
+    age : 21 ,
+    skills : {
+        skill1: 'HTML',
+        skill2: 'CSS',
+        skill3: 'JS',
+        other : {
+            skill4: 'react',
+        },
+    },
+
+};
+
+// TASK3...
+
+function newObject(user) {
+    let newUser = {};
+  for (let filedName in user) {
+    if (typeof user[filedName] === "object") {
+        newUser[filedName] = user[filedName];
+    }
+  }
+  return newUser;
+}
+const newUser = newObject(user);
+
+newUser.name = 'Dato',
+newUser.lastName = 'Megrelishvili',
+newUser.age = '25'
+newUser. skill1 = 'Jango'
+newUser.skills.other.skill4 = 'Java'
+
+console.log(user.name); // Vasili
+console.log(user.lastName); // Vakhtangishvili
+console.log(user.age); // 21
+console.log (user.skills.skill1); // HTML
+console.log(user.skills.skill4); // react
+
+console.log(newUser.name); // Dato
+console.log(newUser.lastName); // Megrelishvili
+console.log(newUser.age); // 25
+console.log(newUser.skill1) // Jango
+console.log ( newUser.skills.other.skill4); // Java
+
+console.log (user);
+console.log(newUser);
+
+
